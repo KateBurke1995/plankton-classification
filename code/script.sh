5bbe61d0c9e2e0ba367b13c8af22457f5d00be5e
apeeyush <peeyushagarwal1994@gmail.com>
2015-05-19 09:13:10 +0000

    Remove web folder and update script

--- code/script.sh
+++ code/script.sh
@@ -3,6 +3,12 @@ wget https://www.dropbox.com/s/koixxmawzl4zl4z/train.zip?dl=1
 wget https://www.dropbox.com/s/9h5sr8ikut814zs/test.zip?dl=1
 wget https://www.dropbox.com/s/p5rcopgs83gyvmk/sampleSubmission.csv.zip?dl=1
 
+# Download augmented training data
+wget https://www.dropbox.com/s/skjwc7ctlpirwnh/train_aug.zip?dl=1
+
+# Install screen
+sudo apt-get install screen
+
 # Install zip, unzip
 sudo apt-get install zip unzip
 
@@ -13,3 +19,9 @@ sudo apt-get install zip unzip
 git clone https://github.com/andreafabrizi/Dropbox-Uploader.git
 chmod +x dropbox_uploader.sh
 ./dropbox_uploader.sh
+
+# Install pylearn2
+# http://deeplearning.net/software/pylearn2/
+git clone git://github.com/lisa-lab/pylearn2.git
+sudo python setup.py develop
+export PYLEARN2_DATA_PATH=/data/lisa/data
